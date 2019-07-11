67991267fcc5497965981d0294acedc306fb6769
lukmir <lukasz.mirocha@fideltronik.com>
2019-07-11 13:07:47 +0200

    next commit

--- WebApplication1/Models/Pie.cs
+++ WebApplication1/Models/Pie.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace PieShop.Models
 {
     public class Pie
-    {
+    {    
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
