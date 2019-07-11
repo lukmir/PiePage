b9267b5308946fecf00bafc3d28460230d0d92aa
lukmir <lukasz.mirocha@fideltronik.com>
2019-07-11 14:15:20 +0200

    adding basic navigation

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -32,5 +32,16 @@ namespace PieShop.Controllers
 
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
     }
 }
